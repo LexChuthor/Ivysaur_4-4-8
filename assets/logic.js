0a3692887e2181da0c2d418d67e6a903006d5198
Lex Chuthor <psc9qb@gmail.com>
2018-10-03 22:46:43 -0400

    added basic html elements and page backbone

---
+++ assets/logic.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBxWllofkCDGCcqO0XRmccGUVejL0M6vTs",
+    authDomain: "workout-tracker-454ba.firebaseapp.com",
+    databaseURL: "https://workout-tracker-454ba.firebaseio.com",
+    projectId: "workout-tracker-454ba",
+    storageBucket: "workout-tracker-454ba.appspot.com",
+    messagingSenderId: "1072910033041"
+  };
+  firebase.initializeApp(config);
